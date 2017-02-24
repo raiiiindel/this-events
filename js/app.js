6fe7ca3733e37379fa6f0c33c0a63d450c4a204f
Rhaynedel Oclit <Rhaynedel Oclit>
2017-02-23 21:48:42 -1000

    this Events finished

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
 //Secret Menu Challenge
 /*Add event listeners to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
+var mcElem = document.getElementsByClassName('name');
+for(var i=0; i<mcElem.length; i++){
+	mcElem[i].addEventListener('click', showMcMenu);
+}
+
+function showMcMenu(){
+	var menuElem = this.querySelectorAll('.menu')[0];
+	if(menuElem.style.display === 'none'){
+		menuElem.style.display = 'block';
+	}else{
+		menuElem.style.display = 'none';
+	}
+}
+
 
 
